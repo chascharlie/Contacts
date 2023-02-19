f3fe403a768052d0f5e27f846630034ec6afb4a4
Charlie <123321721+chascharlie@users.noreply.github.com>
2023-02-19 21:22:08 +0000

    Add comments

--- main.py
+++ main.py
@@ -1,66 +1,72 @@
-import csv,os
-from options import *
+import os # Import os module
+from options import * # Import everything from options
 
-if not os.path.exists("db.csv") or os.path.getsize("db.csv") == 0:
-    with open("db.csv","a+") as write:
-        writer = csv.writer(write)
-        writer.writerow(["Id","FirstName","LastName","Email","Phone","Address"])
+if not os.path.exists("db.csv") or os.path.getsize("db.csv") == 0: # If db.csv either does not exist or its file size is 0 (empty)
+    with open("db.csv","a+") as write: # Open it in append mode as write
+        writer = csv.writer(write) # Write to as CSV
+        writer.writerow(["Id","FirstName","LastName","Email","Phone","Address"]) # Write header to file
 
-while True:
-    clearScreen()
+while True: # While loop; repeat until break
+    clearScreen() # Clear screen
     print('''
 ----------CONTACTS----------
 1. Add new contact
 2. View contact
 3. Edit contact
 4. Delete contact
-5. Exit''')
+5. Exit''') # Present user-friendly options menu
           
-    try:
-        choice = int(input("Enter a choice: "))
-    except:
-        print("Invalid choice, please try again.")
-        input("Press enter to continue...")
-        continue
+    try: # Attempt to execute code
+        choice = int(input("Enter a choice: ")) # Ask user for choice and convert it to integer
+    except: # If an error occurs running try code, likely due to a non-integer item being entered
+        print("Invalid choice, please try again.") # Inform the user
+        input("Press enter to continue...") # Wait for enter to be pressed
+        continue # Restart loop
 
-    if choice == 1:
+    if choice == 1: # If choice is 1 (add contact)
         clearScreen()
         print("--------ADD CONTACT--------")
+        # Prompt user for information, storing it as "blank" if left empty
         firstName = input("First name: ") or 'blank'
         lastName = input("Last name: ") or 'blank'
         email = input("Email: ") or 'blank'
         phone = input("Phone: ") or 'blank'
         address = input("Address: ") or 'blank'
 
-        addContact(firstName,lastName,email,phone,address)        
+        addContact(firstName,lastName,email,phone,address) # Add contact with given data   
 
-    elif choice == 2:
-        rows = sortContacts()
+    elif choice == 2: # Otherwise, if choice is 2 (view contact)
+        rows = sortContacts() # Sort contacts in ascending order based on their full name (first and last)
 
         while True:
             clearScreen()
             print("-------VIEW CONTACT-------")
-            count = 1
-            for row in rows:
-                print(f"{count}. {row[1]} {row[2]}")
-                count += 1
+            count = 1 # Count will number each row
+            for row in rows: # Go through each row
+                print(f"{count}. {row[1]} {row[2]}") # Print count along with first and last names
+                count += 1 # Increment count
 
-            print(f"{count}. Back")
+            print(f"{count}. Back") # Count's last value can be entered to go back
 
             try:
                 choice = int(input("Enter a choice: "))
             except:
+                print("Invalid choice, please try again.")
+                input("Press enter to continue...")
+                continue
+
+            if choice < 1 or choice > count: # If choice is less than 1 or more than count
                 print("Invalid choice, please try again.")
                 input("Press enter to continue...")
                 continue
 
             break
         
-        if choice != count:
-            viewContact(rows[choice-1])
-            input("Press enter to continue...")
+        if choice != count: # If choice is less than count, so it is a number of a row
+            viewContact(rows[choice-1]) # View information in that row
+            input("Press enter to continue...") # Display information until user presses enter
 
-    elif choice == 3:
+    elif choice == 3: # Otherwise, if choice is 3 (edit contact)
         rows = sortContacts()
 
         while True:
@@ -88,14 +94,14 @@ while True:
             break
 
         if choice != count:
-            id = int(rows[choice-1][0])
-            editContact(id)
+            id = int(rows[choice-1][0]) # Get ID from column 1 of chosen row
+            editContact(id) # Edit contact with that ID
 
-    elif choice == 4:
+    elif choice == 4: # Otherwise, if choice is 4 (delete contact)
         rows = sortContacts()
         while True:
             clearScreen()
-            print("-------EDIT CONTACT-------")
+            print("-------DELETE CONTACT-------")
             count = 1
             for row in rows:
                 print(f"{count}. {row[1]} {row[2]}")
@@ -119,12 +125,12 @@ while True:
 
         if choice != count:
             id = int(rows[choice-1][0])
-            deleteContact(id)        
+            deleteContact(id) # Delete contact with that ID        
 
-    elif choice == 5:
-        break
+    elif choice == 5: # Otherwise, if choice is 5
+        break # Break out of loop
 
-    else:
-        print("Invalid choice, please try again.")
+    else: # Otherwise, so none of the other if statements were satisfied
+        print("Invalid choice, please try again.") # Inform user choice is invalid
         input("Press enter to continue...")
-        continue
+        continue # Restart loop
--- options.py
+++ options.py
@@ -1,44 +1,44 @@
-import csv,os
+import csv,os # Import csv and os modules
 
-def addContact(*arg):
+def addContact(*arg): # Procedure addContact
+    with open("db.csv","r") as read: # Open db.csv in read-only mode
+        reader = csv.reader(read,skipinitialspace=True) # Read as CSV file
+        readerList = list(reader) # Convert CSV contents to list
+        try: # Attempt to run
+            id = int(readerList[len(readerList)-1][0])+1 # Set ID for contact
+        except: id = 1 # If error, id set to default 1
+
+    with open("db.csv","a") as write: # Open db.csv in append mode
+        writer = csv.writer(write) # Write to as CSV file
+        row = [id] # ID as first item in row
+        for item in arg: # Each variable passed to function
+            row.append(item) # Append to row
+
+        writer.writerow(row) # Write row to database
+
+def deleteContact(id): # Procedure deleteContact taking in ID
     with open("db.csv","r") as read:
         reader = csv.reader(read,skipinitialspace=True)
         readerList = list(reader)
-        try:
-            id = int(readerList[len(readerList)-1][0])+1
-        except: id = 1
 
-    with open("db.csv","a") as write:
+    index = findIndex(readerList,id) # Find row with ID
+    readerList.pop(index) # Remove row
+
+    with open("db.csv","w") as write: # Open db.csv in write mode
         writer = csv.writer(write)
-        entry = [id]
-        for item in arg:
-            entry.append(item)
+        writer.writerows(readerList) # Overwrite contents of database
 
-        writer.writerow(entry)
-
-def deleteContact(id):
-    with open("db.csv","r") as read:
-        reader = csv.reader(read,skipinitialspace=True)
-        readerList = list(reader)
-
-    index = findIndex(readerList,id)
-    readerList.pop(index)
-
-    with open("db.csv","w") as write:
-        writer = csv.writer(write)
-        writer.writerows(readerList)    
-
-def editContact(id):
+def editContact(id): # Procedure editContact taking in ID
     with open("db.csv","r") as read:
         reader = csv.reader(read,skipinitialspace=True)
         readerList = list(reader)
     
     index = findIndex(readerList,id)
 
-    clearScreen()
+    clearScreen() # Clear screen
     print("-------EDIT CONTACT-------")
     print("Leave a field empty to keep the same.")
-    readerList[index][1] = input("First name: ") or readerList[index][1]
+    readerList[index][1] = input("First name: ") or readerList[index][1] # Ask user for new first name, if left empty will stay the same
     readerList[index][2] = input("Last name: ") or readerList[index][2]
     readerList[index][3] = input("Email: ") or readerList[index][3]
     readerList[index][4] = input("Phone: ") or readerList[index][4]
@@ -48,29 +48,30 @@ def editContact(id):
         writer = csv.writer(write)
         writer.writerows(readerList)
 
-def findIndex(rows,id):
-    for i in range(0,len(rows)):
-        if rows[i][0] == str(id):
-            return i
+def findIndex(rows,id): # Function findIndex taking in rows (2d array) and ID
+    for i in range(0,len(rows)): # Loop i from 0 to length of rows
+        if rows[i][0] == str(id): # If item 0 of row i is equal to ID
+            return i # Return i; the index has been found
         
-def sortContacts():
+def sortContacts(): # Function sortContacts
     with open("db.csv","r") as read:
         reader = csv.reader(read,skipinitialspace=True)
         readerList = list(reader)
-        readerList = readerList[1:len(readerList)]
-        for i in range(0,len(readerList)):
-            for j in range(0,len(readerList)-i-1):
-                fn1 = f"{readerList[j][1]} {readerList[j][2]}"
-                fn2 = f"{readerList[j+1][1]} {readerList[j+1][2]}"
-                if fn1 > fn2:
-                    backup = readerList[j]
-                    readerList[j] = readerList[j+1]
-                    readerList[j+1] = backup
+        readerList = readerList[1:len(readerList)] # Slice out header from readerList
+        # This does a bubble sort of readerList in ascending order of names
+        for i in range(0,len(readerList)): # Loop i from 0 to length of readerList
+            for j in range(0,len(readerList)-i-1): # Loop j from 0 to length of readerList subtract i and 1
+                fn1 = f"{readerList[j][1]} {readerList[j][2]}" # Combine first and last names of first row
+                fn2 = f"{readerList[j+1][1]} {readerList[j+1][2]}" # Combine first and last names of second row
+                if fn1 > fn2: # If full name of first row larger than full name of second row
+                    backup = readerList[j] # Backup first row
+                    readerList[j] = readerList[j+1] # Set first row to contents of second row
+                    readerList[j+1] = backup # Set second row to backed-up contents of first row
 
-    return readerList
+    return readerList # Return sorted list
 
-def viewContact(row):
-    clearScreen()
+def viewContact(row): # Procedure viewContact taking row
+    clearScreen() # Clear screen
     print("-------CONTACT DETAILS-------")
     print(f"First name: {row[1]}")
     print(f"Last name: {row[2]}")
@@ -79,5 +80,5 @@ def viewContact(row):
     print(f"Address: {row[5]}")
 
 
-def clearScreen():
-    os.system('cls' if os.name == 'nt' else 'clear') 
+def clearScreen(): # Procedure clearScreen
+    os.system('cls' if os.name == 'nt' else 'clear') # If on Windows, run cls; else run clear
